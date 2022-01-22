8a2032d5f5362bcc2858d31e089ce4c4bd11b389
Dmitriy Bashkalin <d.bashkalin@gardatech.ru>
2022-01-23 00:41:00 +0300

    add preference pane draft target

---
+++ TestTaskManagerControl/TestTaskManagerControl.h
@@ -0,0 +1,14 @@
+//
+//  TestTaskManagerControl.h
+//  TestTaskManagerControl
+//
+//  Created by Dmitriy Bashkalin on 23.01.2022.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface TestTaskManagerControl : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ TestTaskManagerControl/TestTaskManagerControl.m
@@ -0,0 +1,16 @@
+//
+//  TestTaskManagerControl.m
+//  TestTaskManagerControl
+//
+//  Created by Dmitriy Bashkalin on 23.01.2022.
+//
+
+#import "TestTaskManagerControl.h"
+
+@implementation TestTaskManagerControl
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
