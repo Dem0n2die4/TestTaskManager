3ff57271088adee2c51dc3532a6c454037464a07
Dmitriy Bashkalin <d.bashkalin@gardatech.ru>
2022-01-22 23:43:50 +0300

    empty project

---
+++ TestTaskManager/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TestTaskManager
+//
+//  Created by Dmitriy Bashkalin on 22.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
